b79e32c391ce5533726e8379199e731ce4dfa87f
Cheigarik <pylin@yandex.ru>
2018-09-16 19:55:44 +0300

    Initial Commit

---
+++ bot.py
@@ -0,0 +1,29 @@
+import telebot
+from telebot import apihelper
+
+ip = '37.1.192.213'
+port = '443'
+
+apihelper.proxy = {
+  'https': 'socks5://{}:{}'.format(ip,port)
+}
+
+#main variables
+TOKEN = "624798015:AAFvK2W7CkjEMj56ZVB6c7bYcPLWaDff12o"
+bot = telebot.TeleBot(TOKEN)
+
+
+#handlers
+@bot.message_handler(commands=['start', 'go'])
+def start_handler(message):
+    bot.send_message(message.chat.id, 'Привет, я бот безумного Гарика, молочу в фарш любое информационное мясо')
+
+@bot.message_handler(content_types=['text'])
+def text_handler(message):
+    text = message.text.lower()
+    chat_id = message.chat.id
+    if text == "привет":
+        bot.send_message(chat_id, 'Привет')
+    else:
+        bot.send_message(chat_id, 'Простите, я вас не понял :(')
+bot.polling()
